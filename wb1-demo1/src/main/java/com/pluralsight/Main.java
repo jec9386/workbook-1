c85ca3f27164dbfe086b0b8316dbc1d903e8e338
jec9386 <jerrychandev@gmail.com>
2025-04-14 10:55:48 -0400

    variable and expression added

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -2,11 +2,18 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        String word1 = "Hello";
+
+        String word2 = "World";
+
+        String greeting;
+
+        greeting = word1 + " " + word2 + "!";
 
 
 
 
-        
-        System.out.println("Hello, World!");
+
+        System.out.println(greeting);
     }
 }
