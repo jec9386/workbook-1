fd75eb25ab7bf97deabeee97409f858bbeb0426f
jec9386 <jerrychandev@gmail.com>
2025-04-16 13:09:50 -0400

    DRAFT- Added Loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/Main.java
+++ SandwichShop/src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         //Prompt for size of sandwich
-        System.out.print("What size sandwich do you want?: ");
+        System.out.print("What size sandwich do you want? Regular($5.45) or Larger($8.95): ");
         String size = scanner.nextLine();
 
         double price = 0.0;
@@ -23,20 +23,71 @@ public class Main {
             System.exit(0);
         }
 
+
+        //Prompt user for the loaded or no
+        System.out.printf("Do you want loaded fries(double everything)? Regular Sandwich(+$1.00) and LargeSandwich(+$1.75)\n(Yes/No)?: ");
+        String loaded = scanner.nextLine();
+        if(loaded.equalsIgnoreCase("Yes")){
+            if (size.equalsIgnoreCase("regular")){
+                price += 1.00;
+            }
+            else{
+                price +=1.75;
+            }
+            System.out.println("Sandwich is loaded!");
+        }else{
+            System.out.println("Sandwich is not loaded.");
+        }
+
+/*
+        //Prompt user for size and add the cost to price
+        if(input.equalsIgnoreCase("yes")) {
+            System.out.print("What size sandwich did you order Regular(+$1.00) or Large(+1.75)?: ");
+            String response = scanner.nextLine();
+            if (response.equalsIgnoreCase("Regular")) {
+                price += 1.00;
+            }
+            else if (response.equalsIgnoreCase("Large")) {
+                price += 1.75;
+            }
+            else {
+                System.out.println("Invalid size entered.");
+                System.exit(0);
+            }
+            System.out.println("Double everything added!");
+
+        }
+        else if (input.equalsIgnoreCase("no")) {
+            System.out.println("No loaded fries.");
+
+        }
+        else{
+            System.out.println("Wrong selection.");
+            System.exit(0);
+        }
+
+*/
+
+
+
+
+
         //prompt for age
-        System.out.print("What is your age?: ");
+        System.out.print("Enter age to see if you qualify for a discount?: ");
         int age = scanner.nextInt();
 
         //if else for student and seniors discount
 
         if (age <= 17){
-            System.out.println("Discounted price: $" + String.format("%.2f", price / 1.10));
+            System.out.println("Discounted price: $" + String.format("%.2f", price * .90));
         }
         else if (age >= 65){
-            System.out.println("Discounted price: $" + String.format("%.2f", price / 1.20));
+            System.out.println("Discounted price: $" + String.format("%.2f", price * .80));
         }else {
-            System.out.println("No Discount: $" + String.format("%.2f", price));
+            System.out.println("Sorry no discount final price is: $" + String.format("%.2f", price));
         }
+
+
 
 
     }
