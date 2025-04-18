a067230a9dc75bff0ed62f81215f730f0fbe8c7b
jec9386 <jerrychandev@gmail.com>
2025-04-18 11:49:18 -0400

    Completed RentalCarCalculator

--- PayrollCalculator/src/main/java/com/pluralsight/Payroll.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Payroll.java
@@ -4,7 +4,31 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Payroll {
+    public static void main(String[] args) {
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Get the known values.
+        System.out.print("Please enter the employee name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Please enter hours worked this period: ");
+        float hoursWorked = scanner.nextFloat();
+
+        System.out.print("Please enter base payrate: $ ");
+        double payRate = scanner.nextDouble();
+
+
+        //Calculate the unknown
+        double grossPay = hoursWorked * payRate;
+
+        //Display the results
+        System.out.printf("%S worked hours at a rate of %f and made $%f",name,hoursWorked,payRate);
+    }
+}
+
+    /*
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -37,3 +61,4 @@ public class Payroll {
     }
 
 }
+*/
